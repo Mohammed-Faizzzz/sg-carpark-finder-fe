c4327d8745fb59e28cb837ff901fe171d7ff4b7e
Mohammed-Faizzzz <110959467+Mohammed-Faizzzz@users.noreply.github.com>
2025-07-01 11:07:07 +0100

    Add Analytics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
     const [postcode, setPostcode] = useState('');
@@ -108,6 +109,7 @@ function App() {
                     })}
                 </div>
             )}
+            <Analytics />
         </div>
     );
 }
